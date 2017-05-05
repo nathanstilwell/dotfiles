47dd1f786f4fd7095d19ac47222c8e470cb186a0
Nathan Stilwell <nathan@flow.io>
2017-05-05 19:18:02 -0400

    install WIP

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Make sure dot files are copied to the right place?
 DOTS=~/.dotfiles;
 
 # Check and switch shell to zsh
@@ -6,30 +8,30 @@ DOTS=~/.dotfiles;
 # env | grep SHELL
 
 # if not zsh then,
-# chsh /bin/zsh
+#chsh /bin/zsh
 
 # in case you forgot to clone --recursive
-git submodule init;
-git submodule update;
+# git submodule init;
+# git submodule update;
 
 #
 # Install stuff
 #
 
 # let's crank up some homebrew
-command -v brew > /dev/null || {
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-}
+# command -v brew > /dev/null || {
+#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+# }
 
 # install brew cask
-brew install caskroom/cask/brew-cask
+# brew install caskroom/cask/brew-cask
 
 # install brew bundle
-brew tap Homebrew/bundle
+# brew tap Homebrew/bundle
 
 # then fire up the brew file
-cd $DOTS/install
-brew bundle
+# cd $DOTS/install
+# brew bundle
 
 # link dotfiles
 for file in $DOTS/link/.??*; do
@@ -40,6 +42,20 @@ done
 unset file
 
 # install oh-my-zsh fork
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/nathanstilwell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/nathanstilwell/oh-my-zsh/master/tools/install.sh)"
 
 unset DOTS
+
+
+#
+# Mac Sane Defaults
+
+# turn off unicode glyphs
+# defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Crank up the key repeat
+# defaults write NSGlobalDomain KeyRepeat -int 1
+
+# ask for password after screen saver
+# defaults write com.apple.screensaver askForPassword -int 1
+# defaults write com.apple.screensaver askForPasswordDelay -int 0
