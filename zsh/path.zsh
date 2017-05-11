f54d63eeb0635c9421dbb0bee0afc1c720fcd5b6
Nathan Stilwell <nathan@flow.io>
2017-05-11 13:25:39 -0400

    adding rbenv, because ruby sucks

--- zsh/path.zsh
+++
@@ -1,9 +0,0 @@
-# bins
-export PATH=/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:$PATH
-
-# git
-export PATH=/usr/local/git/bin:$PATH
-export PATH=~/.dotfiles/git-cmd:$PATH
-
-# ??
-export PATH=/usr/X11/bin:$PATH
