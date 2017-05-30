75bbb709e6a06a5b3eb6f2da5c2428b72a68a80c
Nathan Stilwell <nathan@flow.io>
2017-05-30 11:25:47 -0400

    Moving aliases to a topic
    and breaking them up into subjects or whatever

---
+++ aliases/alphabet.zsh
@@ -0,0 +1,28 @@
+#                                 ██      ██         ██                ██                ██
+#                                ████    ░██ ██████ ░██               ░██               ░██
+#                               ██░░██   ░██░██░░░██░██       ██████  ░██       █████  ██████
+#                              ██  ░░██  ░██░██  ░██░██████  ░░░░░░██ ░██████  ██░░░██░░░██░
+#                             ██████████ ░██░██████ ░██░░░██  ███████ ░██░░░██░███████  ░██
+#                            ░██░░░░░░██ ░██░██░░░  ░██  ░██ ██░░░░██ ░██  ░██░██░░░░   ░██
+#                            ░██     ░██ ███░██     ░██  ░██░░████████░██████ ░░██████  ░░██
+#                            ░░      ░░ ░░░ ░░      ░░   ░░  ░░░░░░░░ ░░░░░    ░░░░░░    ░░
+
+
+## Alphabet
+alias a='apm'
+alias b='brew'
+alias d='docker'
+alias f='find . | fzf'
+# alias g='git'
+# alias g='gcloud'
+alias h='heroku'
+alias k='ksdiff'
+# alias k='kubectl'
+alias m='make'
+alias n='npm'
+alias p='python -m SimpleHTTPServer'
+alias v='vim'
+alias r='rbenv'
+alias s='screen -DRU'
+alias t='travis'
+alias y='yarn'
