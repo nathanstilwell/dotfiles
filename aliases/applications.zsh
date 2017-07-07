58119d9094d39206dca1be6b2f6e1ed59e9a6747
Nathan Stilwell <nathan@flow.io>
2017-07-07 15:28:26 -0400

    WIP

--- aliases/applications.zsh
+++ aliases/applications.zsh
@@ -12,6 +12,12 @@ alias mkd='open -a /Applications/Marked\ 2.app'
 command -v hub > /dev/null && alias git='hub'
 alias typ='typings'
 
-# Vim Help
+# Vim edit diff from master
 alias fulldiff='git diff origin/master...HEAD'
 alias vimall='vim `fulldiff --name-only`'
+
+# Atom edit fulldiff
+alias atomall='atom `fulldiff --name-only`'
+
+# I'm working on something here. I want an alias to open all the dirty files
+alias all-dirty-files="git status --porcelain | awk '{print $2}'"
