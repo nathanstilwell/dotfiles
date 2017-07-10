5ec7cebfb8aaa99b03d1c595a36d82ae1ee22b50
Nathan Stilwell <nathanstilwell@gmail.com>
2017-07-10 12:04:07 -0400

    Edit dirty and diff with atom (#11)
    * WIP
    
    * aliases to edit working git stuff with atom

--- aliases/applications.zsh
+++ aliases/applications.zsh
@@ -12,6 +12,15 @@ alias mkd='open -a /Applications/Marked\ 2.app'
 command -v hub > /dev/null && alias git='hub'
 alias typ='typings'
 
-# Vim Help
+# Vim edit diff from master
 alias fulldiff='git diff origin/master...HEAD'
 alias vimall='vim `fulldiff --name-only`'
+
+# Atom edit fulldiff
+alias atomall='atom `fulldiff --name-only`'
+
+# list all dirty files in a git repo
+alias all-dirty-files="git status --porcelain | awk '{print \$2}'"
+
+# edit all dirty files
+alias atomdirty='atom `all-dirty-files`'
