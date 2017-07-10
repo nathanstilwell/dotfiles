9c5a81c5b807d76426a37699c1bffffcdd935236
Nathan Stilwell <nathan@flow.io>
2017-07-10 11:20:59 -0400

    aliases to edit working git stuff with atom

--- aliases/applications.zsh
+++ aliases/applications.zsh
@@ -19,5 +19,8 @@ alias vimall='vim `fulldiff --name-only`'
 # Atom edit fulldiff
 alias atomall='atom `fulldiff --name-only`'
 
-# I'm working on something here. I want an alias to open all the dirty files
-alias all-dirty-files="git status --porcelain | awk '{print $2}'"
+# list all dirty files in a git repo
+alias all-dirty-files="git status --porcelain | awk '{print \$2}'"
+
+# edit all dirty files
+alias atomdirty='atom `all-dirty-files`'
