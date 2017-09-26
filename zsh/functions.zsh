2ba5baf03a4ebdc1ea4791a76c21db2da4b05117
Nathan Stilwell <nathan@flow.io>
2017-09-26 10:01:59 -0400

    function for random sha

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -76,3 +76,7 @@ function profile {
 function stats() {
   history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
+
+function randomSha {
+  head -n 4096 /dev/urandom | openssl sha | xargs echo -n | pbcopy;
+}
