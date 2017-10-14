4eb9ca4d2dac302e342a7e9fbe4f40020f6e6a59
Nathan Stilwell <nathanstilwell@gmail.com>
2017-10-14 17:01:36 -0400

    A few comments about what this does
    Plus losing the cd... commands since I never use them and they feel
    optional

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -1,23 +1,9 @@
-# Changing/making/removing directory
+# Setting up Directory Stacks
+# see http://zsh.sourceforge.net/Intro/intro_6.html for info
+#
+# use `dirs -v` to view the directory stack
+#
 setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
-
-cd () {
-  if   [[ "x$*" == "x..." ]]; then
-    cd ../..
-  elif [[ "x$*" == "x...." ]]; then
-    cd ../../..
-  elif [[ "x$*" == "x....." ]]; then
-    cd ../../../..
-  elif [[ "x$*" == "x......" ]]; then
-    cd ../../../../..
-  elif [ -d ~/.autoenv ]; then
-    source ~/.autoenv/activate.sh
-    autoenv_cd "$@"
-  else
-    builtin cd "$@"
-  fi
-}
-
