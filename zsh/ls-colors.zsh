5c612eaf4e99d663fe3b73c774d8efe0e1ecd71c
Nathan Stilwell <nathanstilwell@gmail.com>
2017-10-16 23:00:44 -0400

    colors

---
+++ zsh/ls-colors.zsh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#         __       ____
+#        /\ \     /\  _`\
+#        \ \ \    \ \,\L\_\
+#         \ \ \  __\/_\__ \
+#          \ \ \L\ \ /\ \L\ \
+#           \ \____/ \ `\____\
+#            \/___/   \/_____/
+
+#   a = black
+#   b = red
+#   c = green
+#   d = brown
+#   e = blue
+#   f = magenta
+#   g = cyan
+#   h = light gray
+#   x = default
+
+export CLICOLOR=1
+export LSCOLORS=excxgxgxBxgxgxgxgxFxFx
+
