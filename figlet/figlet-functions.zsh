c41dabfcc6f32f6d7e532e0e8f4222e90f474a32
Nathan Stilwell <nathanstilwell@gmail.com>
2017-10-16 22:45:35 -0400

    dump the uneeded output

--- figlet/figlet-functions.zsh
+++ figlet/figlet-functions.zsh
@@ -10,17 +10,17 @@
 ##
 
 function __ () {
-  command -v figlet && figlet -f larry3d -w 120 -c "$1" | pbcopy
+  command -v figlet > /dev/null && figlet -f larry3d -w 120 -c "$1" | pbcopy
 }
 
 function _r () {
-  command -v figlet && figlet -f roman -w 120 -c "$1" | pbcopy
+  command -v figlet > /dev/null && figlet -f roman -w 120 -c "$1" | pbcopy
 }
 
 function _3 () {
-  command -v figlet && figlet -f 3d "$1"
+  command -v figlet > /dev/null && figlet -f 3d "$1";
 }
 
 function fig () {
-  command -v figlet && figlet -f "$1" -w 120 -c "$2" | pbcopy;
+  command -v figlet > /dev/null && figlet -f "$1" -w 120 -c "$2" | pbcopy;
 }
