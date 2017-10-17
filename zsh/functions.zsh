16a57c77bb9916e7e10f3710973b318cf60caf1e
Nathan Stilwell <nathanstilwell@gmail.com>
2017-10-16 22:31:15 -0400

    atom package sync

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,26 +9,6 @@ echo " (╯°□°）╯︵ ┻━┻" | pbcopy;
 }
 
 ##
-##    Apps
-##
-
-function atom-save {
-  if [[ -e ~/.atom && -e ~/.atom/bin ]]; then
-    ~/.atom/bin/save-packages;
-  else
-    echo "Atom editor not installed";
-  fi
-}
-
-function atom-install {
-  if [[ -e ~/.atom && -e ~/.atom/bin ]]; then
-    ~/.atom/bin/install-packages;
-  else
-    echo "Atom editor not installed";
-  fi
-}
-
-##
 ##    Utils
 ##
 
