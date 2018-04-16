7f7de3d8b2384c6fd6290503b57c6eb654183e51
Nathan Stilwell <nathan@flow.io>
2018-04-16 07:31:09 -0400

    alias ngrok and http.cat

--- aliases/utilities.zsh
+++ aliases/utilities.zsh
@@ -19,3 +19,14 @@ alias history='fc -l 1'
 
 ## edit dotfiles
 alias dots='vim ~/.dotfiles'
+
+## Check HTTP status codes (with cats)
+function whatcat {
+  if [ -z "$1" ]; then
+cat <<-USAGE
+  usage: whatcat <http status code>
+USAGE
+  else
+    open "https://http.cat/$1";
+  fi
+}
