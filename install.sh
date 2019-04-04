fab74fa21ae8f2ba878649922e9b57272c6330df
Nathan Stilwell <nathan@flow.io>
2019-04-04 18:12:56 -0400

    Replacing link folder situation with a better install script
    Now dotfiles and symlinks will exist in their own topic folders. Files that should be prefaced with a dot will have the extenstion `dotfile` and files to be symlinked with thier given names will have the extension `symlink`

--- install.sh
+++ install.sh
@@ -1,27 +1,66 @@
 #!/bin/bash
 
-# Make sure dot files are copied to the right place?
-DOTS=~/.dotfiles;
+TARGET=$HOME;
+DOTS=$HOME/.dotfiles;
 
-# Check and switch shell to zsh
-# check shell with:
-# env | grep SHELL
+# shellcheck source=/dev/null #[SC1090] I don't care
+source "$DOTS/utils/colors.sh";
 
-# if not zsh then,
-#chsh /bin/zsh
+echo
+echo -e "~~ Replace me with some cool ASCII art ~~";
+echo
 
-# in case you forgot to clone --recursive
-# git submodule init;
-# git submodule update;
+# if not zsh then change shell,
+[ "$SHELL" != "/bin/zsh" ] && {
+  echo "Current user shell is $SHELL, changing user shell to zsh.";
+  # chsh -s /bin/zsh;
+}
 
-#
-# Install stuff
-#
+# link dotfiles (DEPRECATED)
+# for file in $DOTS/link/.??*; do
+#   filename=$(basename "$file");
+#   [[ -e "$file" && -h ~/$filename ]] && rm ~/"$filename";
+#   [[ -e "$file" ]] && ln -s "$file" ~/;
+# done
+# unset file
 
-# let's crank up some homebrew
-# command -v brew > /dev/null || {
-#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-# }
+echo
+echo -e "${bold:?}## Symlinking dotfiles to ~";
+echo -e "--------------------------------------------${bold_off:?}"
+
+for link in "$DOTS"/**/*.dotfile; do
+  filename=$(basename "$link");
+
+  [[ -e $link ]] && {
+    dotfilename=".$(echo "$filename" | cut -d. -f1)";
+    echo -e "Symlinking ${green:?}$filename${stop:?} -> ${green:?}$TARGET/$dotfilename${stop:?}";
+    ln -s "$link" "$TARGET/$dotfilename";
+  };
+done
+
+echo
+echo -e "${bold:?}## Linking symlinks to ~";
+echo -e "--------------------------------------------${bold_off:?}"
+
+for link in "$DOTS"/**/*.symlink; do
+  filename=$(basename "$link");
+
+  [[ -e $link ]] && {
+    symlinkname="$(echo "$filename" | cut -d. -f1)";
+    echo -e "Symlinking ${green:?}$filename${stop:?} -> ${green:?}$TARGET/$symlinkname${stop:?}";
+    ln -s "$link" "$TARGET/$symlinkname";
+  };
+done
+
+echo
+echo -e "${bold:?}## Homebrew";
+echo -e "--------------------------------------------${bold_off:?}"
+command -v brew > /dev/null || {
+  echo "${bold:?}Installing homebrew${stop:?}";
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+} && {
+  echo "Homebrew is already installed";
+}
 
 # install brew cask
 # brew install caskroom/cask/brew-cask
@@ -33,29 +72,21 @@ DOTS=~/.dotfiles;
 # cd $DOTS/install
 # brew bundle
 
-# link dotfiles
-for file in $DOTS/link/.??*; do
-  filename=$(basename "$file");
-  [[ -e "$file" && -h ~/$filename ]] && rm ~/"$filename";
-  [[ -e "$file" ]] && ln -s "$file" ~/;
-done
-unset file
-
-# install oh-my-zsh fork
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/nathanstilwell/oh-my-zsh/master/tools/install.sh)"
-
-unset DOTS
-
-
-#
 # Mac Sane Defaults
+echo
+echo -e "${bold:?}## Setting Mac Defaults";
+echo -e "--------------------------------------------${bold_off:?}"
 
-# turn off unicode glyphs
-# defaults write -g ApplePressAndHoldEnabled -bool false
+echo -e "Turning off unicode glyphs";
+defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Crank up the key repeat
-# defaults write NSGlobalDomain KeyRepeat -int 1
+echo -e "Crank up the key repeat";
+defaults write NSGlobalDomain KeyRepeat -int 1
 
-# ask for password after screen saver
-# defaults write com.apple.screensaver askForPassword -int 1
-# defaults write com.apple.screensaver askForPasswordDelay -int 0
+echo -e "Ask for password after screen saver"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Cleanup
+unset TARGET
+unset DOTS
