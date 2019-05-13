9d528b0f2dc623b73403afe36f292d8bdac3f42c
Nathan Stilwell <nathan@flow.io>
2019-05-13 17:14:16 -0400

    adjustments to key repeat setup

--- install.sh
+++ install.sh
@@ -114,8 +114,9 @@ echo -e "Turning off unicode glyphs";
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 echo -e "Crank up the key repeat";
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 1
 
 echo -e "Ask for password after screen saver"
 defaults write com.apple.screensaver askForPassword -int 1
