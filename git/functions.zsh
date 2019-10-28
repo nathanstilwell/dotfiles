e628cf388ad314f77d3452e4f3f7a3c4243753b2
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2019-10-28 16:32:16 -0400

    prune all remotes

--- git/functions.zsh
+++ git/functions.zsh
@@ -3,7 +3,7 @@ function current_branch() {
 }
 
 function prune {
-  git fetch --prune;
+  git fetch --all --prune;
   git rebase;
   for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`;
     do hub branch -D $branch;
