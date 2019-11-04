92e92667fed12a5ce78a6ce9df90944421f157d9
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2019-11-04 13:38:13 -0500

    alias tweaking

--- npm/functions.zsh
+++ npm/functions.zsh
@@ -3,4 +3,5 @@ function npm-burn {
   # rm package-lock.json;
   [[ -d node_modules ]] && rm -rf node_modules;
   [[ -e package-lock.json ]] && rm package-lock.json;
+  [[ -e yarn.lock ]] && rm yarn.lock;
 }
