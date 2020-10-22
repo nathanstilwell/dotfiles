157867fab49a109bdbb8e6dac191102b875b8203
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-10-22 14:23:07 -0400

    Exporting colors from colors.sh

--- utils/colors.sh
+++ utils/colors.sh
@@ -10,41 +10,41 @@
 #                                \ \_\
 #                                 \/_/
 
-colors_loaded=true;
+export colors_loaded=true;
 
 # Attributes
-bold="\033[1m";
-bold_off="\033[22m";
-blink="\033[5m";
-blink_off="\033[25m";
+export bold="\033[1m";
+export bold_off="\033[22m";
+export blink="\033[5m";
+export blink_off="\033[25m";
 
 # Text colors
-black="\033[30m";
-red="\033[0;31m";
-orange="\033[1;31m";
-olive="\033[0;32m";
-green="\033[1;32m";
-brown="\033[0;33m";
-yellow="\033[1;33m";
-blue="\033[0;34m";
-sky="\033[1;34m";
-magenta="\033[0;35m";
-purple="\033[0;35m";
-aqua="\033[0;36m";
-cyan="\033[0;36m";
-ecru="\033[0;37";
-white="\033[1;37";
+export black="\033[30m";
+export red="\033[0;31m";
+export orange="\033[1;31m";
+export olive="\033[0;32m";
+export green="\033[1;32m";
+export brown="\033[0;33m";
+export yellow="\033[1;33m";
+export blue="\033[0;34m";
+export sky="\033[1;34m";
+export magenta="\033[0;35m";
+export purple="\033[0;35m";
+export aqua="\033[0;36m";
+export cyan="\033[0;36m";
+export ecru="\033[0;37";
+export white="\033[1;37";
 
 
 # Background colors
-b_black="\033[0;40m";
-b_red="\033[0;41m";
-b_green="\033[0;42m";
-b_brown="\033[0;43m";
-b_blue="\033[0;44m";
-b_magenta="\033[0;45m";
-b_cyan="\033[0;46m";
-b_white="\033[0;47m";
+export b_black="\033[0;40m";
+export b_red="\033[0;41m";
+export b_green="\033[0;42m";
+export b_brown="\033[0;43m";
+export b_blue="\033[0;44m";
+export b_magenta="\033[0;45m";
+export b_cyan="\033[0;46m";
+export b_white="\033[0;47m";
 
 # Resets all
-stop="\033[0m";
+export stop="\033[0m";
