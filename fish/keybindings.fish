d1253f1e89aa928efca522549a31443d6e06d8a5
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2021-11-22 16:52:17 -0500

    Still kind of fudging it on fzf

--- fish/keybindings.fish
+++ fish/keybindings.fish
@@ -1,4 +1,3 @@
 ## fzf key binds
-bind \cf __fzf_search_current_dir
 bind \cr __fzf_search_history
 bind \cg __fzf_search_git_log
---
+++ fzf/fzf.fish
@@ -0,0 +1,33 @@
+function __fzf_search_git_log --description "Search the git log of the current git repository. Insert the selected commit hash into the commandline at the cursor."
+    if not git rev-parse --git-dir >/dev/null 2>&1
+        echo '__fzf_search_git_log: Not in a git repository.' >&2
+    else
+        set selected_log_line (
+            # see documentation for git format placeholders at https://git-scm.com/docs/git-log#Documentation/git-log.txt-emnem
+            # %h gives you the abbreviated commit hash, which is useful for saving screen space, but we will have to expand it later below
+            git log --color=always --format=format:'%C(bold blue)%h%C(reset) - %C(cyan)%>(14)%ar%C(reset) %C(normal)%s%C(reset)   %C(yellow)%d%C(reset) %C(dim normal)[%an]%C(reset)' | \
+            fzf --ansi --tiebreak=index
+        )
+        if test $status -eq 0
+            set abbreviated_commit_hash (string split --max 1 " " $selected_log_line)[1]
+            set commit_hash (git rev-parse $abbreviated_commit_hash)
+            commandline --insert $commit_hash
+        end
+    end
+
+    commandline --function repaint
+end
+
+function __fzf_search_history --description "Search command history using fzf. Replace the commandline with the selected command."
+    history merge
+    set command_selected (
+        history --null |
+        fzf --read0 --tiebreak=index --query=(commandline)
+    )
+
+    if test $status -eq 0
+        commandline --replace $command_selected
+    end
+
+    commandline --function repaint
+end
