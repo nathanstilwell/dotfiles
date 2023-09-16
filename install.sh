5c2603b89162b19f6a09471478189a5b4caaa31d
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-09-16 10:38:36 -0400

    updating homebrew install and Brewfile

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ echo -e "${bold:?}## Homebrew";
 echo -e "--------------------------------------------${bold_off:?}"
 command -v brew > /dev/null || {
   echo -e "${bold:?}Installing homebrew${stop:?}";
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 } && {
   echo "Homebrew is already installed";
 }
