5e6c8c7ac89b77df6c0981b3279240f1d0c4a443
Nathan Stilwell <nathan.stilwell@biltcard.com>
2024-02-05 12:01:47 -0500

    adding key binds for fzf.fish

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -16,4 +16,5 @@
 # Ctrl+P -- search processess
 #
 
+# Configure key bindings for patrickf1/fzf.fish
 fzf_configure_bindings --directory=\ct --git_log=\cg --git_status=\cs --history=\cr --processes=\cp
