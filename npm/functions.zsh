51c4e3477cbbd1af3cb9de0660cbbb6fcd9cc966
Nathan Stilwell <nathan.stilwell@biltcard.com>
2024-02-11 21:56:57 -0500

    Removing zsh, bash, and plugins
    I'm cutting down the main branch to be just fish shell. Zsh files will
    live on a zsh branch, bash on a bash branch, etc.

--- npm/functions.zsh
+++
@@ -1,7 +0,0 @@
-function npm-burn {
-  # rm -rf node_modules;
-  # rm package-lock.json;
-  [[ -d node_modules ]] && rm -rf node_modules;
-  [[ -e package-lock.json ]] && rm package-lock.json;
-  [[ -e yarn.lock ]] && rm yarn.lock;
-}
