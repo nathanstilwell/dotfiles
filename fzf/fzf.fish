8af1327f9b24ae113276c66c80d31acf9b6dcf50
Nathan Stilwell <nathan.stilwell@biltcard.com>
2024-03-29 08:40:45 -0400

    moving keybindings around to consolidate

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -1,12 +1,12 @@
 
-#   ██   ██                  ██████   ██               ██        
-#  ░██  ██           ██   ██░█░░░░██ ░░               ░██        
+#   ██   ██                  ██████   ██               ██
+#  ░██  ██           ██   ██░█░░░░██ ░░               ░██
 #  ░██ ██    █████  ░░██ ██ ░█   ░██  ██ ███████      ░██  ██████
-#  ░████    ██░░░██  ░░███  ░██████  ░██░░██░░░██  ██████ ██░░░░ 
-#  ░██░██  ░███████   ░██   ░█░░░░ ██░██ ░██  ░██ ██░░░██░░█████ 
+#  ░████    ██░░░██  ░░███  ░██████  ░██░░██░░░██  ██████ ██░░░░
+#  ░██░██  ░███████   ░██   ░█░░░░ ██░██ ░██  ░██ ██░░░██░░█████
 #  ░██░░██ ░██░░░░    ██    ░█    ░██░██ ░██  ░██░██  ░██ ░░░░░██
-#  ░██ ░░██░░██████  ██     ░███████ ░██ ███  ░██░░██████ ██████ 
-#  ░░   ░░  ░░░░░░  ░░      ░░░░░░░  ░░ ░░░   ░░  ░░░░░░ ░░░░░░  
+#  ░██ ░░██░░██████  ██     ░███████ ░██ ███  ░██░░██████ ██████
+#  ░░   ░░  ░░░░░░  ░░      ░░░░░░░  ░░ ░░░   ░░  ░░░░░░ ░░░░░░
 
 #
 # Ctrl+T -- search directory
@@ -14,7 +14,13 @@
 # Ctrl+S -- search git status
 # Ctrl+R -- search history
 # Ctrl+P -- search processess
+# Ctrl+N -- search npm lifecycle scripts
 #
+
+# capture unbound keys -- see https://fishshell.com/docs/current/cmds/bind.html
+bind '' self-insert
 
 # Configure key bindings for patrickf1/fzf.fish
 fzf_configure_bindings --directory=\ct --git_log=\cg --git_status=\cs --history=\cr --processes=\cp
+
+# Npm life cycle scripts bound to Ctrl+N. see npm/functions.fish
