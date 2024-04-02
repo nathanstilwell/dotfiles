cd257d88fd90a0504f5655c9272e3c6e3a7cf65b
Nathan Stilwell <nathan.stilwell@biltcard.com>
2024-04-01 20:10:10 -0400

    fzf: Spicing up staging files
    I modified the function (from PatrickF1/fzf.fish[^1]) that uses fzf to
    view staged files to escape parenthesis (which is necessary for working
    in NextJS) and adds "git add" before the list of files to quickly stage
    them. I bound the new function to the same key bind as the old one
    (Ctrl+s).

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -21,6 +21,9 @@
 bind '' self-insert
 
 # Configure key bindings for patrickf1/fzf.fish
-fzf_configure_bindings --directory=\ct --git_log=\cg --git_status=\cs --history=\cr --processes=\cp
+fzf_configure_bindings --directory=\ct --git_log=\cg --history=\cr --processes=\cp
+
+# Bind Ctrl+S to stage files in git
+bind \cs __fzf_git_stage_files
 
 # Npm life cycle scripts bound to Ctrl+N. see npm/functions.fish
--- git/functions.fish
+++ git/functions.fish
@@ -11,3 +11,51 @@ function prune --description "Delete git branches that have lost remotes"
   end
 end
 
+#
+# Shamelessly stolen from _fzf_search_git_status.fish[^1]
+# for the purpose of staging files the contain parentheses in
+# their paths. This function depends on PatrickF1/fzf.fish being
+# installed.
+#
+# [1]: https://github.com/PatrickF1/fzf.fish/blob/dfdf69369bd3a3c83654261f90363da2aa1db8c9/functions/_fzf_search_git_status.fish
+#
+function __fzf_git_stage_files --description "Fuzzy search for files to stage"
+  if git rev-parse --git-dir >> /dev/null 2>&1
+    set -f preview_cmd '_fzf_preview_changed_file {}'
+    if set --query fzf_diff_highlighter
+        set preview_cmd "$preview_cmd | $fzf_diff_highlighter"
+    end
+
+    set -f selected_paths (
+        # Pass configuration color.status=always to force status to use colors even though output is sent to a pipe
+        git -c color.status=always status --short |
+        _fzf_wrapper --ansi \
+            --multi \
+            --prompt="Git Status> " \
+            --query=(commandline --current-token) \
+            --preview=$preview_cmd \
+            --nth="2.." \
+            $fzf_git_status_opts
+    )
+
+    if test $status -eq 0
+        # Escape parentheses in paths
+        #set -f escaped_paths (echo $selected_paths | sed 's/(/\\\(/g' | sed 's/)/\\\)/g');
+        set -f cleaned_paths
+
+        for path in $selected_paths
+            set -f escaped (echo $path | sed 's/(/\\\(/g' | sed 's/)/\\\)/g');
+
+            if test (string sub --length 1 $escaped) = R
+                # path has been renamed and looks like "R LICENSE -> LICENSE.md"
+                # extract the path to use from after the arrow
+                set --append cleaned_paths (string split -- "-> " $escaped)[-1]
+            else
+                set --append cleaned_paths (string sub --start=4 $escaped)
+            end
+        end
+        commandline --current-token --replace -- "git add "(string join " " $cleaned_paths);
+    end
+    commandline -f repaint
+  end
+end
