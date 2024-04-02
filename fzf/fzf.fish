190dc6bf79d5db01f01b342fd1b9ff5e8837dad9
Nathan Stilwell <nathan.stilwell@biltcard.com>
2024-04-01 20:14:28 -0400

    moving npm keybinding to fzf.fish (where they belong)

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -26,4 +26,5 @@ fzf_configure_bindings --directory=\ct --git_log=\cg --history=\cr --processes=\
 # Bind Ctrl+S to stage files in git
 bind \cs __fzf_git_stage_files
 
-# Npm life cycle scripts bound to Ctrl+N. see npm/functions.fish
+# Npm life cycle scripts bound to Ctrl+N.
+bind \cN __fzf_npm_life_cycle_scripts
