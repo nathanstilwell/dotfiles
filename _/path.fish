7b2f49bc58597dbe2b6a2ee39d27bc5f6e2f4c9e
Nathan Stilwell <nathanstilwell@gmail.com>
2024-04-14 15:01:54 -0400

    adding basic paths with `fish_add_path`

--- _/path.fish
+++ _/path.fish
@@ -1,4 +1,4 @@
 # These are basic bins that need to be loaded first, so
 # they can be overridden by applications later
-set -U fish_user_paths /bin /usr/sbin /sbin /usr/local/bin /usr/bin
 
+fish_add_path -g /bin /usr/sbin /sbin /usr/local/bin /usr/bin
