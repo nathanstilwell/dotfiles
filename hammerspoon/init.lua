c2e2453311008bdce74eb933e630dadd1b047f24
Nathan Stilwell <nathan.stilwell@biltrewards.com>
2024-09-26 10:04:00 -0400

    linting hammerspoon. removing vscode. adding zoom

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,16 +2,16 @@
 -- Shared functions
 --
 function resizeWindow(dw, dh, dx, dy)
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
 
-    f.w = dw and max.w * dw or f.w
-    f.h = dh and max.h * dh or f.h
-    f.x = dx and max.x + (max.w * dx) or f.x
-    f.y = dy and max.y + (max.h * dy) or f.y
-    win:setFrame(f)
+  f.w = dw and max.w * dw or f.w
+  f.h = dh and max.h * dh or f.h
+  f.x = dx and max.x + (max.w * dx) or f.x
+  f.y = dy and max.y + (max.h * dy) or f.y
+  win:setFrame(f)
 end
 
 function fullScreenWindow(win)
@@ -27,7 +27,7 @@ function fullScreenWindow(win)
 end
 
 function moveWindowToDisplay(d)
-  return function ()
+  return function()
     local displays = hs.screen.allScreens()
     local win = hs.window.focusedWindow()
     win:moveToScreen(displays[d], false, true)
@@ -39,24 +39,24 @@ end
 -- Window Movement in Monitor
 --
 
-hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "Up", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "Up", function()
   local win = hs.window.focusedWindow()
   fullScreenWindow(win)
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "Up", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "Up", function()
   resizeWindow(1.0, 0.5, 0, 0)
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "Down", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "Down", function()
   resizeWindow(1.0, 0.5, 0, 0.5)
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "Left", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "Left", function()
   resizeWindow(0.5)
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "Right", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "Right", function()
   resizeWindow(0.5, nil, 0.5)
 end)
 
@@ -64,73 +64,73 @@ end)
 -- Move to Monitor
 --
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "1", moveWindowToDisplay(1))
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "2", moveWindowToDisplay(2))
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "3", moveWindowToDisplay(3))
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "1", moveWindowToDisplay(1))
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "2", moveWindowToDisplay(2))
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "3", moveWindowToDisplay(3))
 
 --
 -- Focus Applications
 --
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "B", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "B", function()
   hs.application.launchOrFocus("Google Chrome")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "C", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "C", function()
   hs.application.launchOrFocus("1Password")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "D", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "D", function()
   hs.application.launchOrFocus("Google Chrome Canary")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "E", function ()
-  hs.application.launchOrFocus("Visual Studio Code")
-end)
-
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "G", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "G", function()
   hs.application.launchOrFocus("Figma")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "H", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "H", function()
   hs.application.launchOrFocus("Hammerspoon")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "I", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "I", function()
   -- No idea why this is necessary
   hs.application.launchOrFocusByBundleID("com.culturedcode.ThingsMac")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "L", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "L", function()
   hs.application.launchOrFocus("Slack")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "M", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "M", function()
   hs.application.launchOrFocus("Messages")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "N", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "N", function()
   hs.application.launchOrFocus("Linear")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "P", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "P", function()
   hs.application.launchOrFocus("Music")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "R", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "R", function()
   hs.application.launchOrFocus("Arc")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "T", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "T", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "W", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "W", function()
   hs.application.launchOrFocus("WezTerm")
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "Z", function()
+  hs.application.launchOrFocus("zoom.us")
 end)
 
 -- Sleep Displays
 
-hs.hotkey.bind({"cmd", "ctrl", "alt"}, "7", function ()
+hs.hotkey.bind({ "cmd", "ctrl", "alt" }, "7", function()
   hs.execute("pmset displaysleepnow")
 end)
